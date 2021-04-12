5befaddac2058f3a33d34ac73e88684ea5399e1f
Kirill <blayner0027@gmail.com>
2021-04-12 21:16:09 +0400

    Выполнил задание 11

---
+++ Task11/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+using Task11.Models;
+
+namespace Task11.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class StudentController : Controller
+	{
+		[HttpGet]
+		public string Index()
+		{
+			string queryString = "SELECT Surname, Name, Patronomic FROM Students WHERE Is_Orphant = 0";
+			string connectionString = @"Server=DESKTOP-25EV0KI\DEV;Database=College_Db;Trusted_Connection=True;";
+			List<Student> students = new();
+			using (SqlConnection connection = new(connectionString))
+			{
+				SqlCommand command = new(queryString, connection);
+				connection.Open();
+				SqlDataReader reader = command.ExecuteReader();
+				while (reader.Read())
+				{
+					students.Add(new Student() { Name = (string)reader[1], Patronomic = (string)reader[2], Surname = (string)reader[0] });
+				}
+			}
+			return JsonConvert.SerializeObject(students);
+		}
+		[HttpDelete]
+		public void Delete()
+		{
+			string queryString = "DELETE Students WHERE Is_Orphant = 1";
+			string connectionString = @"Server=DESKTOP-25EV0KI\DEV;Database=College_Db;Trusted_Connection=True;";
+			List<Student> students = new();
+			SqlConnection connection = new(connectionString);
+			SqlCommand command = new(queryString, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			while (reader.Read())
+			{
+				students.Add(new Student() { Name = (string)reader[1], Patronomic = (string)reader[2], Surname = (string)reader[0] });
+			}
+		}
+	}
+}
---
+++ Task11/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Task11.Models
+{
+	public class Student
+	{
+		public string Surname { get; set; }
+		public string Name { get; set; }
+		public string Patronomic { get; set; }
+
+		public override string ToString() => $"{Surname} {Name} {Patronomic}";
+	}
+}
