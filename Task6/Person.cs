258e3337d753117d8da59c22c999f874aa4d2591
Kirill <blayner0027@gmail.com>
2021-04-12 20:21:05 +0400

    Выполнил задание 6

---
+++ Task6/Person.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task6
+{
+	internal class Person
+	{
+		/// <summary>
+		/// Имя.
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Возраст.
+		/// </summary>
+		public int Age { get; set; }
+
+		/// <summary>
+		/// Сортировать возраст по убыванию 
+		/// </summary>
+		/// <param name="peoples"></param>
+		/// <returns></returns>
+		public static IEnumerable<Person> OrderByAgePeaple(IEnumerable<Person> peoples) =>
+			peoples.OrderByDescending(x => x.Age);
+
+		/// <summary>
+		/// Сгруппировать по возрасту
+		/// </summary>
+		/// <param name="peoples"></param>
+		/// <returns></returns>
+		public static IEnumerable<IGrouping<int, Person>> GroupPeopleByAge(IEnumerable<Person> peoples)
+		{
+			return from p in peoples
+				   group p by p.Age;
+		}
+
+		/// <summary>
+		/// Отфильтровать только совершеннолетних
+		/// </summary>
+		/// <param name="peoples">Коллекция</param>
+		/// <returns></returns>
+		public static IEnumerable<Person> FiltringAdventurePeoples(IEnumerable<Person> peoples) =>
+			peoples.Where(x => x.Age >= 18);
+
+		/// <summary>
+		/// Найти в списке по имени
+		/// </summary>
+		/// <param name="people">Коллекция</param>
+		/// <param name="name">Имя, которое вы хотите найти</param>
+		/// <returns></returns>
+		public static Person FindPeole(IEnumerable<Person> people, string name) =>
+			people.First(x => x.Name == name);
+
+		public override string ToString() => Name + ' ' + Age;
+	}
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task6
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			List<Person> listPersons = new List<Person>()
+			{
+				new Person(){Name = "Kirill", Age = 19},
+				new Person(){Name = "Andrew", Age = 20},
+				new Person(){Name = "July", Age = 19},
+				new Person(){Name = "Nik", Age = 17},
+				new Person(){Name = "Loly", Age = 18},
+				new Person(){Name = "Artem", Age = 17}
+			};
+			Console.WriteLine("Сгруппированы по возрасту:");
+			foreach (IGrouping<int, Person> item in Person.GroupPeopleByAge(listPersons))
+			{
+				Console.WriteLine(item.Key);
+				foreach (Person item2 in item)
+				{
+					Console.WriteLine(item2.Name);
+				}
+				Console.WriteLine();
+			}
+
+			Console.WriteLine("Отсортированные по возрастанию:");
+			foreach (Person i in Person.OrderByAgePeaple(listPersons))
+			{
+				Console.WriteLine(i + "\n");
+			}
+		}
+	}
+}
