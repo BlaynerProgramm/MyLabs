c25b522852c8adcf677dae8a19b7a7412e88fdb4
Kirill <blayner0027@gmail.com>
2021-04-12 19:33:27 +0400

    Выполнил задание 4

---
+++ Task4/ArraySort.cs
@@ -0,0 +1,58 @@
+﻿namespace Task4
+{
+	internal class ArraySort
+	{
+		/// <summary>
+		/// Сравнение указанных типов
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public delegate bool Сompare<T>(T a, T b);
+		/// <summary>
+		/// Делегат для события EndSort
+		/// </summary>
+		/// <param name="message"></param>
+		public delegate void MessageArray(string message);
+
+		/// <summary>
+		/// Сортировка методом пузырька
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="array">Массив</param>
+		/// <param name="comparer">Метод сравнения</param>
+		public static void Sort<T>(T[] array, Сompare<T> comparer)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				for (int j = i + 1; j < array.Length; j++)
+				{
+					if (!comparer(array[j], array[i]))
+					{
+						T temp = array[i];
+						array[i] = array[j];
+						array[j] = temp;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Сравнение чисел
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns>true - если a > b</returns>
+		public static bool IntСompare(int a, int b) => a > b;
+
+
+		/// <summary>
+		/// Сравнение строк
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns>true - если a > b</returns>
+		public static bool StringСompare(string a, string b) => a.Length > b.Length;
+	}
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task4
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			int[] a = new int[] { 1, 3, 5, 7, 3 };
+			ArraySort.Sort(a, ArraySort.IntСompare);
+			for (int i = 0; i < a.Length; i++)
+			{
+				Console.WriteLine(a[i]);
+			}
+		}
+	}
+}
