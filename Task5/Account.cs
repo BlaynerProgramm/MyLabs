dc076d7ddc38fb0ed800c3278bc19da6ba7edc9e
Kirill <blayner0027@gmail.com>
2021-04-12 20:17:27 +0400

    Выполнил задание 5

---
+++ Task5/Account.cs
@@ -0,0 +1,24 @@
+﻿namespace Task5
+{
+	internal class Account
+	{
+		/// <summary>
+		/// Идентификатор аккаунта
+		/// </summary>
+		public int AccountId { get; set; }
+		/// <summary>
+		/// Почта
+		/// </summary>
+		public string Email { get; set; }
+		/// <summary>
+		/// Полное имя
+		/// </summary>
+		public string FullName { get; set; }
+		/// <summary>
+		/// Дата последнего входа
+		/// </summary>
+		public System.DateTime DateLastEnter { get; set; }
+
+		public override string ToString() => $"Id: {AccountId}\nEmail: {Email}\nName: {FullName}\nDate Last Enter: {DateLastEnter}";
+	}
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text.Json;
+
+namespace Task5
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			List<Account> listAccounts = new()
+			{
+				new Account() { AccountId = 1, DateLastEnter = DateTime.Now, Email = "gtefwFassd@norbit.ru", FullName = "Филя Пупкин" },
+				new Account() { AccountId = 2, DateLastEnter = new DateTime(2020, 10, 3), Email = "Ar34asd@norbit.ru", FullName = "Геннадий Пупкин" }
+			};
+
+			File.WriteAllText(ConfigurationManager.AppSettings.Get("Folder"), JsonSerializer.Serialize(listAccounts, new JsonSerializerOptions() { WriteIndented = true }));
+			Account[] accounts = JsonSerializer.Deserialize<Account[]>(File.ReadAllText(ConfigurationManager.AppSettings.Get("Folder")));
+			foreach (Account account in accounts)
+			{
+				Console.WriteLine(account);
+			}
+		}
+	}
+}
