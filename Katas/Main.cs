08670754b6648445cb6d8612ffe09f457b4d5b9c
Kirill <blayner0027@gmail.com>
2021-11-27 17:11:55 +0400

    SpinWords

--- Katas/Main.cs
+++ Katas/Main.cs
@@ -25,5 +25,35 @@ namespace Katas
         /// <param name="s"></param>
         /// <returns></returns>
         public static int FindShort(string s) => s.Split().Min(x => x.Length);
+
+        /// <summary>
+        /// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+        /// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" ) => returns "This is rehtona test"
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string SpinWords(string str)
+        {
+            string[] strArray = str.Split();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (strArray[i].Length >= 5)
+                {
+                    char[] spinWord = strArray[i].ToCharArray();
+                    Array.Reverse(spinWord);
+                    sb.Append(spinWord);
+                }
+                else
+                {
+                    sb.Append(strArray[i]);
+                }
+
+                sb.Append(" ");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
