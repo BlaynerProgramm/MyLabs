c6e386b43378ddc5473fd98d963c53de8cb3e4ad
Kirill <blayner0027@gmail.com>
2021-11-27 17:13:43 +0400

    find_it

--- Katas/Main.cs
+++ Katas/Main.cs
@@ -55,5 +55,19 @@ namespace Katas
 
             return sb.ToString().TrimEnd();
         }
+
+        /// <summary>
+        /// Given an array of integers, find the one that appears an odd number of times.
+        ///There will always be only one integer that appears an odd number of times.
+        ///Examples
+        ///[7] should return 7, because it occurs 1 time (which is odd).
+        ///[0] should return 0, because it occurs 1 time(which is odd).
+        ///[1,1,2] should return 2, because it occurs 1 time(which is odd).
+        ///[0,1,0,1,0] should return 0, because it occurs 3 times(which is odd).
+        ///[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time(which is odd).
+        /// </summary>
+        /// <param name="seq"></param>
+        /// <returns></returns>
+        public static int find_it(int[] seq) => seq.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count()).SingleOrDefault(x => x.Value % 2 != 0).Key;
     }
 }
