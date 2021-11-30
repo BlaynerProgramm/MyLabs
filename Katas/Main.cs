bd419f6cad7f35cd7136decd36b5a34c38e4839b
Kirill <blayner0027@gmail.com>
2021-11-30 15:12:19 +0400

    UInt32ToIP

--- Katas/Main.cs
+++ Katas/Main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Katas
@@ -69,5 +70,25 @@ namespace Katas
         /// <param name="seq"></param>
         /// <returns></returns>
         public static int find_it(int[] seq) => seq.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count()).SingleOrDefault(x => x.Value % 2 != 0).Key;
+
+        /// <summary>
+        /// Take the following IPv4 address: 128.32.10.1
+
+        ///This address has 4 octets where each octet is a single byte (or 8 bits).
+
+        ///1st octet 128 has the binary representation: 10000000
+        ///2nd octet 32 has the binary representation: 00100000
+        ///3rd octet 10 has the binary representation: 00001010
+        ///4th octet 1 has the binary representation: 00000001
+
+        ///So 128.32.10.1 == 10000000.00100000.00001010.00000001
+
+        ///Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
+
+        ///Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static string UInt32ToIP(uint ip) => IPAddress.Parse(ip.ToString()).ToString();
     }
 }
